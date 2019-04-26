24e6428ae5ab7607b026c4d94c4716826f5a65fc
liu253928127 <253928127@qq.com>
2019-04-26 20:57:06 +0800

    update application

---
+++ ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
@@ -0,0 +1,14 @@
+package com.ihrm.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenwu.liu.o
+ */
+@SpringBootApplication
+public class CompanyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyApplication.class, args);
+    }
+}
