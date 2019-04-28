74144e54b0e607b25f2ed0c8a18cdcfa66027089
liu253928127 <253928127@qq.com>
2019-04-28 09:16:36 +0800

    update 添加启动company模块所需配置

---
+++ ihrm_company/src/main/java/com/ihrm/company/common/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.ihrm.company.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wenwu.liu.o
+ */
+@Configuration
+@MapperScan("com.ihrm.company.dao")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     *
+     * @return PaginationInterceptor
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
